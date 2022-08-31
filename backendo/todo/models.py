d1cfe4460a91a1226e7cac229d4945d1f78b469a
tanakou0226 <79981775+tanakou0226@users.noreply.github.com>
2022-09-01 02:34:45 +0900

    update models.py
    modelsにuuidや作成日を追加しました

--- backendo/todo/models.py
+++ backendo/todo/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import uuid
 
 
 class Todo(models.Model):
-    todo = models.CharField('Todo', max_length=255, blank=False)
-    contents = models.CharField('content', max_length=255)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    todo = models.CharField('Todo', max_length=255, blank=False, null=False)
+    contents = models.TextField('content', max_length=255, blank=False, null=False)
+    created_data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.todo
